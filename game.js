edf30480d0d2d7e99ef26b91555295f4657ab569
Lizzie Shipton <lizzie.shipton@gmail.com>
2016-11-29 20:32:04 -0800

    [fix] update win logic

--- game.js
+++ game.js
@@ -78,8 +78,9 @@ var checkForWins = function(newBoard) {
       }
   }
   for(var combo of winningCombos) {
-    if(combo[0] === compareYCombos || combo[0] === compareXCombos) {
-      console.log("YOU WIN!!!")
+    if( (combo[0] === compareYCombos || combo[0] === compareYCombos.reverse) ||
+        (combo[0] === compareXCombos || combo[0] === compareXCombos.reverse) {
+      console.log("YOU WIN!!!");
     }
   }
 };
